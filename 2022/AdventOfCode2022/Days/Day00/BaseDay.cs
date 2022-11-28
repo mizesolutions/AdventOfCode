1e9782a104eafd4eb74df152b6203a1eef84acb0
Brian Mize <brian.r.mize@gmail.com>
2022-11-28 09:37:24 -0800

    Setting up for 2022 and a little warm up with 2015

---
+++ 2022/AdventOfCode2022/Days/Day00/BaseDay.cs
@@ -0,0 +1,57 @@
+﻿using AdventOfCode2022.Infrastructure.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+namespace AdventOfCode2022.Days.Day00
+{
+    public class BaseDay
+    {
+        public RenderInput Input { get; set; }
+        public List<string> FileInput { get; set; }
+        public int Result1 { get; set; }
+        public int Result2 { get; set; }
+
+        public BaseDay(string day, bool hasInput)
+        {
+            if (hasInput)
+            {
+                try
+                {
+                    Input = new RenderInput(day);
+                    FileInput = Input.FileToList();
+                    Result1 = 0;
+                    Result2 = 0;
+                }
+                catch (FileNotFoundException fe)
+                {
+                    Console.WriteLine(fe.Message);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void PrintCurrentMethod()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(1);
+
+            Console.WriteLine($"  {sf.GetMethod().Name}");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void PrintCurrentClass()
+        {
+            Console.WriteLine($"--==<  {this.GetType().Name}  >==--");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public void PrintResults(int results)
+        {
+            Console.WriteLine($"      Result: {results}\r\n");
+        }
+    }
+}
---
+++ 2022/AdventOfCode2022/Infrastructure/Services/RenderInput.cs
@@ -0,0 +1,85 @@
+﻿using AdventOfCode2022.Properties;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2022.Infrastructure.Services
+{
+    public class RenderInput
+    {
+        public string InputPath { get; set; }
+        public List<int> IntList { get; set; }
+        public int[,] IntMatrix { get; set; }
+        public List<int[,]> IntMatrixList { get; set; }
+
+        public RenderInput(string inputName)
+        {
+            if (File.Exists(Resources.ResourceManager.GetString(inputName)))
+            {
+                this.InputPath = Resources.ResourceManager.GetString(inputName);
+            }
+            else
+            {
+                throw new FileNotFoundException($"Error: {inputName}.txt not found \r\n");
+            }
+            IntList = new();
+            IntMatrix = new int[6, 6];
+            IntMatrixList = new(200);
+        }
+
+        public List<string> FileToList()
+        {
+            List<string> output = new();
+
+            using (StreamReader file = new(InputPath))
+            {
+                var line = "";
+                while ((line = file.ReadLine()) != null)
+                {
+                    output.Add(line);
+                }
+            }
+            return output;
+        }
+
+        public void MixedDataRender()
+        {
+            var result = File.ReadAllLines(InputPath);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(result[i]))
+                    continue;
+                if (result[i].Contains(','))
+                {
+                    IntList = result[0].Split(',').Select(int.Parse).ToList();
+                }
+                else
+                {
+                    if (result[i][0] == ' ')
+                        result[i] = result[i].Substring(1).Replace("  ", " ");
+                    else
+                        result[i] = result[i].Replace("  ", " ");
+                    int[] list = result[i].Split(" ").Select(int.Parse).ToArray();
+                    for (int row = 1, col = 1; row < IntMatrix.GetLength(1); row++)
+                    {
+                        if (IntMatrix[row, 1] == 0 && IntMatrix[row, 2] == 0)
+                        {
+                            foreach (var x in list)
+                            {
+                                IntMatrix[row, col] = x;
+                                col++;
+                            }
+                            row = IntMatrix.GetLength(1);
+                        }
+                    }
+                    if (IntMatrix[5, 4] > 0 || IntMatrix[5, 5] > 0)
+                    {
+                        IntMatrixList.Add(IntMatrix);
+                        IntMatrix = new int[6, 6];
+                    }
+                }
+            }
+        }
+    }
+}
