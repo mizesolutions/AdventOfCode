7c13eccb95d58ed257a75540935b01eb5106de56
Brian Mize <bmize@advantageiq.com>
2023-12-06 07:34:31 -0800

    Setup day 04

---
+++ 2023/AdventOfCode2023/Days/day04.cs
@@ -0,0 +1,25 @@
+﻿namespace AdventOfCode2023.Days
+{
+    public class Day04 : BaseDay
+    {
+
+        public Day04(string day, bool hasInput) : base(day, hasInput)
+        {
+        }
+
+        public override void PuzzleOne()
+        {
+            PrintCurrentMethod();
+
+
+
+            PrintResults(Result1);
+        }
+
+        public override void PuzzleTwo()
+        {
+            PrintCurrentMethod();
+            PrintResults(Result2);
+        }
+    }
+}
