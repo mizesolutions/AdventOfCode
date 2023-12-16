167bdfe13d17477e00dddd9b53887ce6a0c0b704
Brian Mize <bmize@advantageiq.com>
2023-12-15 16:06:03 -0800

    day15

---
+++ 2023/AdventOfCode2023/Days/day15.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Reflection.Emit;
+
+namespace AdventOfCode2023.Days
+{
+    public class Day15 : BaseDay
+    {
+        public List<string> SeqList { get; set; }
+        public OrderedDictionary[] Lenses { get; set; }
+
+        public Day15(string day, bool hasInput) : base(day, hasInput)
+        {
+            SeqList = new List<string>();
+            Lenses = new OrderedDictionary[256];
+            for (var i = 0; i < Lenses.Length; i++)
+            {
+                Lenses[i] = new OrderedDictionary();
+            }
+        }
+
+        public override void PuzzleOne()
+        {
+            PrintCurrentMethod();
+
+            SeqList = FileInput.FirstOrDefault().Split(',').ToList();
+            foreach (var seq in SeqList)
+            {
+                Result1 += GetValue(seq);
+            }
+
+            PrintResults(Result1);
+        }
+
+        public override void PuzzleTwo()
+        {
+            PrintCurrentMethod();
+
+            foreach (var seq in SeqList)
+            {
+                var label = seq.Substring(0, 2);
+                var boxNum = GetValue(label);
+                var lens = 0;
+
+                if (seq.Contains('='))
+                {
+                    lens = int.Parse(seq.Substring(seq.IndexOf('=') + 1));
+                }
+                Operation(boxNum, label, lens);
+            }
+
+            PrintResults(Result2);
+        }
+
+
+        private long GetValue(string seq)
+        {
+            long curr = 0;
+            foreach (char c in seq)
+            {
+                curr += (int)c;
+                curr *= 17;
+                curr %= 256;
+            }
+            return curr;
+        }
+
+        private void Operation(long index, string lable, int lens)
+        {
+            if (lens == 0 && Lenses[index] != null && Lenses[index].Contains(lable))
+            {
+                Lenses[index].Remove(lable);
+            }
+            else
+            {
+                if (Lenses[index] != null && Lenses[index].Contains(lable))
+                {
+                    Lenses[index][lable] = lens;
+                }
+                else
+                {
+                    Lenses[index].Add(lable, lens);
+                }
+            }
+        }
+
+    }
+}
--- 2023/AdventOfCode2023/Infrastructure/Services/DayRunner.cs
+++ 2023/AdventOfCode2023/Infrastructure/Services/DayRunner.cs
@@ -27,8 +27,23 @@ namespace AdventOfCode2023.Infrastructure.Services
                 //{ "day06", new Day06("day06", IsInput)},
                 //{ "day07", new Day07("day07", IsInput)},
                 //{ "day08", new Day08("day08", IsInput)},
-                { "day09", new Day09("day09", IsInput)},
+                //{ "day09", new Day09("day09", IsInput)},
                 //{ "day10", new Day10("day10", IsInput)},
+                //{ "day11", new Day11("day11", IsInput)},
+                //{ "day12", new Day12("day12", IsInput)},
+                //{ "day13", new Day13("day13", IsInput)},
+                //{ "day14", new Day14("day14", IsInput)},
+                { "day15", new Day15("day15", IsInput)},
+                //{ "day16", new Day16("day16", IsInput)},
+                //{ "day17", new Day17("day17", IsInput)},
+                //{ "day18", new Day18("day18", IsInput)},
+                //{ "day19", new Day19("day19", IsInput)},
+                //{ "day20", new Day20("day20", IsInput)},
+                //{ "day21", new Day21("day21", IsInput)},
+                //{ "day22", new Day22("day22", IsInput)},
+                //{ "day23", new Day23("day23", IsInput)},
+                //{ "day24", new Day24("day24", IsInput)},
+                //{ "day25", new Day25("day25", IsInput)}
             };
         }
 
